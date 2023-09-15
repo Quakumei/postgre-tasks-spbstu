b553b05e4e5b7b1e2ba4991b3934829277a558d4
Ilya Tampio <quakumei@gmail.com>
2023-09-16 02:52:20 +0300

    fix: lab3 - minor fix

--- pg3/queries/01_view_service_total_more_than_2700.sql
+++ pg3/queries/01_view_service_total_more_than_2700.sql
@@ -1,4 +1,4 @@
-CREATE VIEW service_total_more_than_2700 AS
+CREATE OR REPLACE VIEW service_total_more_than_2700 AS
 WITH service_total_cost AS (
     SELECT
         service_id,
@@ -23,29 +23,8 @@ WHERE
     total > 2700;
 
 -- Test query
-WITH service_total_cost AS (
-    SELECT
-        service_id,
-        SUM(
-            CASE WHEN cars.is_foreign THEN
-                cost_foreign
-            ELSE
-                cost_our
-            END) AS total
-    FROM
-        works
-        LEFT JOIN services ON services.id = works.service_id
-        LEFT JOIN cars ON cars.id = works.car_id
-    GROUP BY
-        service_id)
-    CREATE VIEW service_total_more_than_2700 AS
-    SELECT
-        *
-    FROM
-        service_total_cost;
-
 SELECT
     *
 FROM
-    service_total_cost;
+    service_total_more_than_2700;
 
